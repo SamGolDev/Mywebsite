2284498e4161bfb03c13d1a0b3a25922f70c61ed
Sergey <fesachedidit@gmail.com>
2022-06-20 17:45:46 +0300

    Updates

---
+++ js/fullpage.js
@@ -0,0 +1,8 @@
+new fullpage('#fullpage', {
+    //options here
+    autoScrolling:true,
+    scrollHorizontally: true,
+    navigation: true,
+    navigationPosition: 'right',
+    scrollingSpeed: 800
+});
---
+++ js/fullpage/fullpage.app.js
@@ -0,0 +1,9 @@
+new fullpage('#fullpage', {
+   autoScrolling: true,
+   scrollingSpeed: 800,
+   navigation: true,
+   fitToSection: true,
+   navigationTooltips: ['Home', 'About Me', 'Skills'],
+   fixedElements: '.sidepanel',
+});
+
